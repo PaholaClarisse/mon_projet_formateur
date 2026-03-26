7a2dd5f64256604ccbc2d7216667c5b1983bce2a
PaholaClarisse <paholamartinelagos@gmail.com>
2026-03-26 21:38:22 +0100

    new commit

---
+++ chanter.py
@@ -0,0 +1,5 @@
+print("presentation:")
+nom = input("quel est votre nom?:")
+age = int(input("quel est votre age?:"))
+print(f"bonjour, je m'appelle {nom}, et j'ai {age} ans.")
+# ceci c'est pour mieux cerne les notion de branche sur git et github
